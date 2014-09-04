d8dbe420160a6ffc1c6194fd7ebfcb035d9c72b9
mwaaas <francismwangi152@gmail.com>
2014-09-04 07:20:36 +0300

    initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+author: mwas
+email: francismwangi152@gmail.com
+	
+This projects uses the inbuild swicht and bulbs to do its functionalty
+	It can be used  as a test of microcontroller if its working
+	
+	workflow:
+		
+	
+*/
+#include "edxtm4c123gh6pm.h"
+int main(void){
+	
+}
